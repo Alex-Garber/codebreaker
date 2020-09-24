1b2bfe4563bf73b4c3340f9d11a6baa10158b1b9
Alex Garber <alexgarber6@gmail.com>
2020-09-24 11:13:07 -0600

    i

--- src/edu/cnm/deepdive/codebreaker/model/Code.java
+++ src/edu/cnm/deepdive/codebreaker/model/Code.java
@@ -8,7 +8,7 @@ public class Code {
 
   public Code(String pool, int length, Random rng) {
     secret = new char[length];
-    for (int i = 0; i <  secret.length; i++ ) {
+    for (int i = 0; i < secret.length; i++) {
       secret[i] = pool.charAt(rng.nextInt(pool.length()));
     }
 
@@ -17,6 +17,51 @@ public class Code {
   @Override
   public String toString() {
     return new String(secret);
+  }
+
+  public class Guess {
+
+    private static final String STRING_FORMAT = "{text: \"%s}\" , correct: %d' close: %d}";
+    private String text;
+    private final int correct;
+    private final int close;
+
+    public Guess(String text) {
+      this.text = text;
+      int correct = 0;
+      int close = 0;
+      for (int i = 0; 1 < secret.length; i++) {
+        char current = secret[i];
+        int position = text.indexOf(current);
+        if (i == position) {
+
+          correct++;
+        } else if (position >= 0) {
+          close++;
+        }
+
+      }
+      this.correct = correct;
+      this.close = close;
+
+    }
+
+    public String getText() {
+      return text;
+    }
+
+    public int getCorrect() {
+      return correct;
+    }
+
+    public int getClose() {
+      return close;
+    }
+
+    @Override
+    public String toString() {
+      return String.format(STRING_FORMAT, text, correct, close);
+    }
   }
 
 }
