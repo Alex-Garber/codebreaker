609c6eee8bded81a2c94ce3bd85406e19af0aff2
Alex Garber <alexgarber6@gmail.com>
2020-10-09 09:40:22 -0600

    Started Javadoc comments

--- src/edu/cnm/deepdive/codebreaker/model/Code.java
+++ src/edu/cnm/deepdive/codebreaker/model/Code.java
@@ -9,10 +9,20 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+/**
+ * Contains the randomly generated code.
+ */
 public class Code {
 
   private final char[] secret;
 
+  /**
+   * Generates and randomizes the pool and length of the secret code.
+   *
+   * @param pool Allowed Characters in the secret.
+   * @param length Total length of secret code.
+   * @param rng Randomizes the secret code.
+   */
   public Code(String pool, int length, Random rng) {
     secret = new char[length];
     for (int i = 0; i < secret.length; i++) {
@@ -26,6 +36,9 @@ public class Code {
     return new String(secret);
   }
 
+  /**
+   * Represents a single attempt of the users guess.
+   */
   public class Guess {
 
     private static final String STRING_FORMAT = "{text: \"%s}\" , correct: %d' close: %d}";
@@ -33,6 +46,10 @@ public class Code {
     private final int correct;
     private final int close;
 
+    /**
+     * Computes how many of the characters in the users guess that are correct.
+     * @param text
+     */
     public Guess(String text) {
       this.text = text;
       int correct = 0;
@@ -83,14 +100,23 @@ public class Code {
       return letterMap;
     }
 
+    /**
+     * Returns the text of this instance.
+     */
     public String getText() {
       return text;
     }
 
+    /**
+     * Returns the number of correct in the users guess that are also in the secret code.
+     */
     public int getCorrect() {
       return correct;
     }
 
+    /**
+     * Returns the number of close in the users guess that are also in the secret code .
+     */
     public int getClose() {
       return close;
     }
